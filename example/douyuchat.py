ad1693583180297f07c77cecdd0f42fad9d92360
Frank <fengxuting@gmail.com>
2017-01-01 16:52:03 +0800

    danmu save redis

--- example/douyuchat.py
+++ example/douyuchat.py
@@ -6,15 +6,25 @@
 [nodejs](https://github.com/yingnansong/douyujs)
 window下cmd乱码解决：chcp 65001
 [windows cmd命令显示UTF8设置](http://www.liangcuntu.com/windows_cmd_utf8)
+https://www.github.com/fffy2366/pydouyu
+
+[Redis 命令参考](http://redisdoc.com/)
+
+ZRANGE douyuchatset 0 -1 WITHSCORES
+hget douyuchathash b8116d914b5b48ce7700010000000000
 '''
 from douyu.chat.room import ChatRoom
 import redis
 from os import system
+import datetime
+import time
 
 r = redis.Redis(host='localhost', port=6379, db=0, password='db2016')
 
 
 def on_chat_message(msg):
+    # print msg.to_text()
+    #el@=/uid@=98050310/nn@=liangcuntu/cid@=b8116d914b5b48ce8cec000000000000/level@=1/gid@=-9999/rg@=4/rid@=593076/txt@=测试/type@=chatmsg/ic@=avanew@Sface@S201612@S28@S08@Sed9e85a68b5fa100dc5ece9902ca9c93/ct@=2/
     # o = '[%s]:%s' % (msg.attr('nn'), msg.attr('txt'))
     s = "{0} :{1}".format(msg.attr('nn'), msg.attr('txt'))
 
@@ -22,10 +32,17 @@ def on_chat_message(msg):
     print s
 
 
-# 保存入redis
-# r.publish('douyu', s)
-# r.set('douyu',s)
+    # 按日期保存入redis
+    # r.publish('douyu', s)
+    # r.set('douyu',s)
+    cid = msg.attr('cid')
+    created_at = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    now = int(time.time())
 
+    r.zadd("douyuchatset",cid,now)
+
+    v = {"nn":msg.attr('nn'),"txt":msg.attr('txt')}
+    r.hset("douyuchathash",cid,v)
 
 def run():
     # 彡彡九
---
+++ test/redisTest.py
@@ -0,0 +1,47 @@
+#!bin/env python
+# -*-coding:utf8-*-
+import redis
+import datetime
+import time
+
+print 'Hello World!'
+
+l = [1,2,5,4,3]
+
+print l
+
+t = (1,2,4)
+
+print t
+
+# 连接redis
+
+r = redis.Redis(host='localhost', port=6379, db=0, password='db2016')
+
+# redis string
+r.set('test','2017 hello')
+print r.get('test')
+
+r.zadd("listtest","key",90)
+# print r.get("listtest")
+
+#redis list
+# r.lpush("pushname","pushvalue")
+print r.rpop("pushname")
+
+# redis hash map
+r.hset("maptest:2017-01-01","key1","value2")
+r.hset("maptest:2017-01-02","key1","value3")
+print r.hget("maptest:2017-01-02","key1")
+
+
+# created_at = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+created_at = datetime.datetime
+
+print created_at
+
+
+
+# 获得当前时间时间戳
+now = int(time.time())  #->这是时间戳
+print now
